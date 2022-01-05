97b2c37834dfdab1fa1338cc5bb8537ba9636f6a
Eduardo Reyes <ereyesoyarzo@gmail.com>
2022-01-05 09:35:29 -0300

    initial server config

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const app = express();
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer, gql } = require('apollo-server-express');
 const cors = require('cors');
 const dotEnv = require('dotenv');
 
 // set environment variables
 dotEnv.config();
 
+// cors
+app.use(cors());
+
 // body parser middleware
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=> {
+// apollo server
+const typeDefs = gql`
+    type Query {
+        greetings: String
+    }
+`;
+
+const resolvers = {};
+
+// server
+async function startApolloServer(typeDefs, resolvers) {
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({app, path: '/graphql'});
+
+    const PORT = process.env.PORT || 3000;
+    await new Promise((resolve,reject)=> app.listen(PORT,resolve))
     console.log(`Server listening on port: ${PORT}`);
-});
+    console.log(`Graphql endpoint : ${apolloServer.graphqlPath}`);
+}
+
+startApolloServer(typeDefs,resolvers);
