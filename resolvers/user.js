f91122a4a8c10bcc090ad9e78c388657027daa72
Eduardo Reyes <ereyesoyarzo@gmail.com>
2022-01-05 13:00:09 -0300

    signup mutation

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,4 +1,6 @@
 const { users, tasks } = require('../constants/index')
+const User = require('../database/models/user')
+const bcrypt = require('bcryptjs')
 
 module.exports = {
     Query: {
@@ -6,6 +8,25 @@ module.exports = {
         user: (_, { id }) => users.find(user => user.id === id),
     },
     Mutation: {
+        signup: async (_, { input }) => {
+            try{
+                const { name, email, password } = input
+                const user = await User.findOne({ email })
+
+                if(user) throw new Error('email already in use')
+
+                const hashedPassword = await bcrypt.hash(password, 12)
+                const newUser = new User({ name,email,password: hashedPassword })
+                const result = await newUser.save()
+
+                return result
+            }catch(error) {
+                console.log(error)
+
+                throw error
+            }
+
+        },
     },
     User: {
         tasks: parent => {
--- typeDefs/user.js
+++ typeDefs/user.js
@@ -6,11 +6,23 @@ module.exports = gql`
         user(id: ID!): User
     }
 
+    input signupInput {
+        name: String!,
+        email: String!,
+        password: String!
+    }
+
+    extend type Mutation {
+        signup(input: signupInput): User
+    }
+
 
     type User {
         id: ID!,
         name: String!,
         email: String!,
-        tasks: [Task!]
+        tasks: [Task!],
+        createdAt: String!,
+        updatedAt: String!
     }
 `
